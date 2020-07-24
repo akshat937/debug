770cf718bb6d88d464f5d487ec60d02eda4fd410
akshat937 <akshatraghuvanshi27@gmail.com>
2020-07-24 18:51:23 +0800

    debugging

--- sketch.js
+++ sketch.js
@@ -78,12 +78,13 @@ function draw(){
     platform.display();
     //log6.display();
     slingshot.display();    
+    console.log(bird);
 }
 
 function mouseDragged(){
-    //if (gameState!=="launched"){
+    if (gameState!=="launched"){
         Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
-    //}
+    }
 }
 
 
@@ -93,8 +94,12 @@ function mouseReleased(){
 }
 
 function keyPressed(){
-    if(keyCode === 32){
+    if(keyCode === 32&& bird.body.speed < 1){
        slingshot.attach(bird.body);
+       bird.trajectory = [];
+       Matter.Body.setPosition(bird.body,{x: 200, y: 50});
+       Matter.Body.setAngle(bird.body,0);
+       gameState = "onSling";
     }
 }
 
